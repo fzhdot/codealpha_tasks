1917122373c5183d64bd15b2d01722f072bc9b79
fzhdot <habibi.fatimazahrae@etu.uae.ac.ma>
2025-07-06 15:13:32 +0200

    Supprimer le dossier CodeAlpha_SimpleEcommerce Store

--- CodeAlpha_SimpleEcommerce Store/users/templatetags/follow_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from users.models import Follow
-
-register = template.Library()
-
-@register.filter
-def is_following(user, target_user):
-    return Follow.objects.filter(follower=user, following=target_user).exists()
